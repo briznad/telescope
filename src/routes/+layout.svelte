5c9618d5ef3ffde92e5cffaad19f29a8a95682f7
Brad Mallow <briznad@gmail.com>
2023-09-11 21:55:32 -0700

    make site PWAable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -62,7 +62,15 @@
 
 
 <svelte:head>
-	<meta name="theme-color" media="(prefers-color-scheme: light)" content="#45aeeb" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#45aeeb">
+	<meta name="apple-mobile-web-app-title" content="Telescope">
+	<meta name="application-name" content="Telescope">
+	<meta name="msapplication-TileColor" content="#45aeeb">
+	<meta name="theme-color" content="#45aeeb">
 </svelte:head>
 
 
